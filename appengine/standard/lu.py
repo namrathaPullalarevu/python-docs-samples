d787c4471229c6af2ae89fd5ebc7dee06f85ac70
namrathaPullalarevu <49633932+namrathaPullalarevu@users.noreply.github.com>
2019-09-16 22:02:19 +0530

    create lu.py

---
+++ appengine/standard/lu.py
@@ -0,0 +1,38 @@
+import pprint
+
+import scipy
+
+import scipy.linalg   # SciPy Linear Algebra Library
+
+import numpy as np
+
+
+a = np.matrix([ [1, 2, 3], [2, 3, 4], [1, 2, 5] ])
+
+k = scipy.array([ [8, 2, 9], [4, 9, 4], [6, 7, 9] ])
+
+print("Secret key is ")
+
+print(k)
+
+P, L, U = scipy.linalg.lu(k)
+
+
+print ("A:")
+
+#pprint.pprint(k)
+
+
+#print( "P:")
+
+#pprint.pprint(P)
+
+
+print ("L:")
+
+pprint.pprint(L)
+
+
+print ("U:")
+
+pprint.pprint(U)
